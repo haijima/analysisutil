43ba5ca578ce46db920a6333f5b79879972e523d
haijima <k.haijima@gmail.com>
2024-07-15 20:56:01 +0900

    feat: Add astutil/comment.go

---
+++ astutil/comment.go
@@ -0,0 +1,36 @@
+package astutil
+
+import (
+	"go/ast"
+	"go/token"
+	"strings"
+)
+
+func WalkCommentGroup(fset *token.FileSet, files []*ast.File, fn func(node ast.Node, cg *ast.CommentGroup) bool) {
+	for _, file := range files {
+		for n, cgs := range ast.NewCommentMap(fset, file, file.Comments) {
+			for _, cg := range cgs {
+				if !fn(n, cg) {
+					break
+				}
+			}
+		}
+	}
+}
+
+func HasCommentPrefix(c *ast.Comment, prefix string) bool {
+	return strings.HasPrefix(c.Text, "// "+prefix) || strings.HasPrefix(c.Text, "//"+prefix)
+}
+
+func GetCommentVerb(c *ast.Comment, prefix string) (string, string, bool) {
+	if !HasCommentPrefix(c, prefix) {
+		return "", "", false
+	}
+	s := strings.TrimPrefix(c.Text, "// "+prefix)
+	s = strings.TrimPrefix(s, "//"+prefix)
+	if s[0] != ':' {
+		return "", "", false
+	}
+	v, arg, _ := strings.Cut(s[1:], " ")
+	return v, arg, v != ""
+}
